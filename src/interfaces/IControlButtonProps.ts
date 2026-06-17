d096948a51e5576bf0a235b91f3c0f8bf61136b1
apastori <alfonso.pastorimata@gmail.com>
2026-06-17 09:43:25 -0300

    NewButtonComponentsPropsInterfaces

---
+++ src/interfaces/IControlButtonProps.ts
@@ -0,0 +1,5 @@
+export interface IControlButtonProps {
+  label: string
+  onClick: () => void
+  disabled?: boolean
+}
---
+++ src/interfaces/IEditingToggleProps.ts
@@ -0,0 +1,5 @@
+export interface IEditingToggleProps {
+  label: string
+  checked: boolean
+  onChange: (checked: boolean) => void
+}
